5f65de6ade8c013518ceb98f98213c4fd4e68b56
dkalchenko01 <dkalchenko@kse.org.ua>
2024-11-21 20:08:30 +0200

    finished top 10 medals

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group(required=True)
 
 group.add_argument('--medals', nargs=2, help='top 10 medalists of selected country')
+group.add_argument('--total', nargs=1, type=int, help='country medals in this year')
 parser.add_argument('--output', nargs=1, type=str, help='file to save output')
 args = parser.parse_args()
 
 if __name__ == '__main__':
     if args.medals:
         top_10_medals(categories, rows, args.medals[0], args.medals[1])
+
 
--- medals.py
+++ medals.py
@@ -1,3 +1,35 @@
+def count_medals(medals_dict):
+    total_gold = 0
+    total_silver = 0
+    total_bronze = 0
+    for name in medals_dict:
+        if 'Gold' in medals_dict[name]:
+            total_gold += medals_dict[name]['Gold']
+        elif 'Silver' in medals_dict[name]:
+            total_silver += medals_dict[name]['Silver']
+        else:
+            total_bronze += medals_dict[name]['Bronze']
+
+    print(f'Total gold: {total_gold}, silver: {total_silver}, bronze: {total_bronze}')
+
+def check_year(rows, year):
+    valid_years = {row[9] for row in rows}
+    if str(year) not in valid_years:
+        print('No olympics this year')
+        return False
+    return True
+
+def check_country(rows, team):
+    valid_teams = {row[6] for row in rows}
+    valid_nocs = {row[7] for row in rows}
+    if team not in (valid_teams or valid_nocs):
+        print('This country does not exict')
+        return False
+    return True
+
+
+
+
 def top_10_medals(header, rows, team, year):
     NAME = header.index('Name')
     TEAM = header.index('Team')
@@ -5,25 +37,29 @@ def top_10_medals(header, rows, team, year):
     YEAR = header.index('Year')
     SPORT = header.index('Sport')
     MEDAL = header.index('Medal')
-
     sportsman_total = {}
     sportsman_medals = {}
-
+    if not check_year(rows, year) or not check_country(rows, team):
+        exit()
     for row in rows:
         if (team == row[TEAM] or team == row[NOC] or team in row[TEAM].split('/')) and year == row[YEAR]:
             if row[MEDAL] != 'NA':
                 if row[NAME] not in sportsman_medals:
-                    sportsman_medals[row[NAME]] = {'discipline': None, 'Gold' : 0, 'Silver' : 0, 'Bronze' : 0}
-                sportsman_medals[row[NAME]][row[MEDAL]] += 1
-                sportsman_medals[row[NAME]]['discipline'] = row[SPORT]
+                    sportsman_medals[row[NAME]] = {'Discipline': None}
+                    if row[MEDAL] not in sportsman_medals[row[NAME]]:
+                        sportsman_medals[row[NAME]][row[MEDAL]] = 1
+                    else:
+                        sportsman_medals[row[NAME]][row[MEDAL]] += 1
+                sportsman_medals[row[NAME]]['Discipline'] = row[SPORT]
                 if row[NAME] not in sportsman_total:
                     sportsman_total[row[NAME]] = 0
                 sportsman_total[row[NAME]] += 1
     top_10 = sorted(sportsman_total.items(), key=lambda x: x[1], reverse=True)[:10]
-
-    for tuple in top_10:
-        name = tuple[0]
-        print(f'{name},  {sportsman_medals[name]['discipline']},  Gold: {sportsman_medals[name]['Gold']},  Silver: {sportsman_medals[name]['Silver']} ,  Bronze: {sportsman_medals[name]['Bronze']}')
+    for result in top_10:
+        print(result[0])
+        for key, value in sportsman_medals[result[0]].items():
+            print(f'   {key}: {value}')
+    count_medals(sportsman_medals)
 
 
 
