3de7ce075c6a0c206d5a37cc909ebc80548860ee
dkalchenko01 <dkalchenko@kse.org.ua>
2024-11-21 16:05:35 +0200

    top 10 medals, created sportsman dictionary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
-from lib2to3.pgen2.tokenize import group
+import argparse
+from medals import top_10_medals
 
 def open_file():
     with open("athlete_events.csv", "r") as file:
@@ -10,7 +11,18 @@ def open_file():
             rows.append(row)
     return header, rows
 
+
 categories, rows = open_file()
 
+
+parser = argparse.ArgumentParser()
+
+group = parser.add_mutually_exclusive_group(required=True)
+
+group.add_argument('--medals', nargs=2, help='top 10 medalists of selected country')
+parser.add_argument('--output', nargs=1, type=str, help='file to save output')
+args = parser.parse_args()
+
 if __name__ == '__main__':
-    print(categories, rows[81])
+    if args.medals:
+        top_10_medals(categories, rows, args.medals[0], args.medals[1])
