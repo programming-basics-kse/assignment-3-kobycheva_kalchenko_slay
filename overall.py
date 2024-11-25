2433ee25c9dbf6531a75b27b06c4c89d2adb4594
Mariia Kobycheva <mkobycheva@kse.org.ua>
2024-11-25 08:17:16 +0200

    finished overall.py function, it works!!

--- overall.py
+++ overall.py
@@ -3,14 +3,23 @@ from validation import check_country, create_indexes
 def overall(header, rows, teams):
     NAME, TEAM, NOC, YEAR, SPORT, MEDAL = create_indexes(header)
     medals_by_years = {}
+    output = ""
     for team in teams:
+        if not check_country(rows, team):
+            exit()
         for row in rows:
-            if row[TEAM] == team:
-                medals_by_years[team] = {}
-                if row[MEDAL] != 'NA':
-                    if row[NAME] not in medals_by_years[team]:
-                        medals_by_years[team] = {row[YEAR]: 1}
+            if (row[TEAM] == team or row[NOC] == team) and row[MEDAL] != 'NA':
+                if team not in medals_by_years:
+                    medals_by_years[team] = {row[YEAR]: 1}
+                else:
+                    if row[YEAR] not in medals_by_years[team]:
+                        medals_by_years[team][row[YEAR]] = 1
                     else:
                         medals_by_years[team][row[YEAR]] += 1
-    print(medals_by_years)
-    print(teams)
+    for team in medals_by_years:
+        team_list = medals_by_years[team]
+        top_year = sorted(team_list.items(), key=lambda x: x[1], reverse=True)[:1]
+        for key, value in top_year:
+            output += f"The most successful year for {team} was {key} with {value} medals \n"
+    print(output)
+    return output
--- validation.py
+++ validation.py
@@ -1,7 +1,7 @@
 def check_year(rows, year):
     valid_years = {row[9] for row in rows}
     if str(year) not in valid_years:
-        print('No Olympics this year')
+        print(f'No Olympics on year {year}')
         return False
     return True
 
@@ -11,7 +11,7 @@ def check_country(rows, team):
     valid_nocs = {row[7] for row in rows}
     if team in valid_teams or team in valid_nocs:
         return True
-    print('This country does not exist')
+    print(f'Country {team} does not exist')
     return False
 
 def create_indexes(header):
