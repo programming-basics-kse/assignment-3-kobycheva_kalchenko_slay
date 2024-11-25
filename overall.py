b446f4a8a18b90b75a3704cb82ae4e53399b6912
Mariia Kobycheva <mkobycheva@kse.org.ua>
2024-11-25 13:59:44 +0200

    changed overall.py so you can use it in interactive

--- overall.py
+++ overall.py
@@ -1,9 +1,8 @@
 from validation import check_country, create_indexes
 
-def overall(header, rows, teams):
+def overall_create_dict(header, rows, teams):
     NAME, TEAM, NOC, YEAR, SPORT, MEDAL = create_indexes(header)
     medals_by_years = {}
-    output = ""
     for team in teams:
         if not check_country(rows, team):
             exit()
@@ -16,6 +15,10 @@ def overall(header, rows, teams):
                         medals_by_years[team][row[YEAR]] = 1
                     else:
                         medals_by_years[team][row[YEAR]] += 1
+        return medals_by_years
+
+def overall_count_overall(medals_by_years):
+    output = ""
     for team in medals_by_years:
         team_list = medals_by_years[team]
         top_year = sorted(team_list.items(), key=lambda x: x[1], reverse=True)[:1]
