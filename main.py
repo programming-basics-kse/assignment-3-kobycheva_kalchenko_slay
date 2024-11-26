e9a7f70385de7f3fcbdf99505c08c9db83ca2bb0
dkalchenko01 <dkalchenko@kse.org.ua>
2024-11-26 15:15:39 +0200

    added top function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import csv
 import argparse
 from medals import return_top_10_medals
+from top import best_participant
 from total import total
 from overall import count_overall
-from interactive import first_participation, interactive
+from interactive import interactive
 from validation import check_country
 
 
@@ -26,6 +27,7 @@ group.add_argument('--medals', nargs=2, help='top 10 medalists of selected count
 group.add_argument('--total', nargs=1, type=int, help='country medals in this year')
 group.add_argument('--overall', nargs='+', type=str, help='the most productive year for this country')
 group.add_argument('--interactive', nargs='*', help='country statistics')
+group.add_argument('--top', nargs='+', help='best participant of certain age group')
 parser.add_argument('--output', nargs=1, type=str, help='file to save output')
 args = parser.parse_args()
 result = None
@@ -40,11 +42,15 @@ if __name__ == '__main__':
     elif args.overall:
         result = count_overall(categories, rows, args.overall)
 
+    elif args.top:
+        result = best_participant(categories, rows, args.top)
+
     elif not args.interactive:
         args.interactive = input('Please, enter a country to get its statistics: ')
         while not check_country(rows, args.interactive):
             args.interactive = input('Please, enter valid country to get its statistics: ')
         result = interactive(categories, rows, args.interactive)
+
 
     if args.output:
         with open(args.output[0], 'w') as results_file:
